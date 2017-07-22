8338349c9e01c15538a4515a0edd1caceff5fb12
Grace Mazzarella <Grace Mazzarella>
2017-07-22 04:04:04 -0400

    stuff

--- structure/views.py
+++ structure/views.py
@@ -4,19 +4,19 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 
-from .forms import TakeDeckstring
+#from .forms import TakeDeckstring
 
 # Create your views here.
-def get_deckstring(request):
-    if request.method == 'POST':
-        form = TakeDeckstring(request.POST)
-        if form.is_valid():
-            return HttpResponseRedirect('PLACEHOLDER')
+#def get_deckstring(request):
+#    if request.method == 'POST':
+#        form = TakeDeckstring(request.POST)
+#        if form.is_valid():
+#            return HttpResponseRedirect('PLACEHOLDER')
 
-    else:
-        form = TakeDeckstring()
+#    else:
+#        form = TakeDeckstring()
 
-    return render(request, 'input_deckstring.html', {'form': form})
+#    return render(request, 'input_deckstring.html', {'form': form})
 
 def index(request):
     return HttpResponse("Hello, welcome to this shitty app.")
