e8f9adc8edcb32007f560cf0e6ed2449f1cf6a62
jian13579 <jianhlu018@gmail.com>
2017-07-22 16:56:04 +0800

    added models

--- structure/models.py
+++ structure/models.py
@@ -4,3 +4,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Card(models.Model):
+    """
+    This is the name of each of the cards in the hand
+    """
+    card = models.Charfield(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Simulator(models.Model):
+    """
+    This is the model which will set up the information that will be dumped
+    into the SQL
+    """
+    deckcode = models.CharField(max_length=200)
+    decklist = models.CharField(max_length=200)
+    order = models.Charfield(max_length=200)
+    opponent = models.Charfield(max_length=200)
+    ##Maybe change the many to many to a single charfield depending on how things
+    ##look in the actual database
+    hand = models.ManyToManyField(Card, through='Card')
+    user_selection = models.ManyToManyField(Card, through='Card')
